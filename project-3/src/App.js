00424bdb26f54d0f583ca759f53369fe193456b8
khushi gupta <guptakhushi622003@gmail.com>
2023-09-11 03:17:21 +0530

    fixed some bugs in project-3

---
+++ project-3/src/App.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <App1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ project-3/src/app.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import './App.css';
+
+const Button = styled.button `
+  background-color =black;
+  pa
+`
+
+function App1() {
+  return (
+    <div>
+      <button>this</button>
+    </div>
+  );
+}
+
+export default App1;
